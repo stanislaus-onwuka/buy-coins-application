d78973e43fa3043aa522fde13df767431c6aa2e1
stanislaus-onwuka <kachi2505@yahoo.com>
2021-05-30 19:55:29 +0000

    View User page implemented

---
+++ js/showProfile.js
@@ -0,0 +1,137 @@
+const userId = JSON.parse(sessionStorage.getItem("user"));
+const repoCount = document.querySelectorAll(".repo-count");
+const userProfile = document.querySelector(".user-profile")
+const userRepositories = document.querySelector(".user-repositories");
+const userAvatar = document.querySelector(".avatar-icon-img");
+
+const githubUrl = 'https://api.github.com/graphql'
+
+const token = "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"
+
+const auth = {Authorization: 'bearer ' + token};
+
+
+const query = 
+`query SearchUser($queryString: String!) {
+    user(login: $queryString) {
+      avatarUrl
+      login
+      name
+      bio
+      repositories(first: 20) {
+        totalCount
+        edges {
+          node {
+            id
+            name
+            forkCount
+            description
+            updatedAt
+            stargazerCount
+            primaryLanguage {
+              color
+              name
+            }
+          }
+        }
+        
+      }
+    }
+}   
+`
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+    axios.post(
+        githubUrl,
+        { 
+            query: query,
+            variables: { "queryString": userId}
+        }, 
+        {headers: auth}
+    )
+    .then((response)=>{
+        const { user } = response.data.data;
+
+        repoCount.forEach((count)=>{
+            count.innerHTML = `${user.repositories.totalCount}`
+        })
+
+        // Update menu avatar
+        userAvatar.src = `${user.avatarUrl}`;
+
+        const userProfileContent = `
+            <div class="user-img">
+                <img src="${user.avatarUrl}"/>
+                <div class="set-emotion">
+                    <img src="/assets/icons/ph_smiley.svg">
+                </div>
+            </div>
+            <div class="user-details">
+                <h3 class="name">${user.name}</h3>
+                <h4 class="username">${user.login}</h4>
+                <p class="bio">${user.bio}</p>
+            </div>
+        `
+
+        // Add to User profile
+        userProfile.innerHTML = userProfileContent;
+
+
+        // Repositories data
+        const repoList = document.createElement("ul");
+        
+        repoList.className="repo-list";
+
+        let {edges} = user.repositories;
+
+        edges.map((repository)=>{
+
+            let repoListItem = document.createElement("li");
+            repoListItem.setAttribute("class","repo")
+            repoListItem.setAttribute("id",repository.node.id)
+
+            // Changing to standard date format
+            let updated_at_time = new Date(repository.node.updatedAt)
+
+            const repositoryDetails = `
+                <div class="repo-details">
+                    <h3 class="repo-name">${repository.node.name}</h3>
+                    <p class="repo-details">${repository.node.description === null ? "" : repository.node.description}</p>
+                    <div class="repo-info">
+                        <div style="display: ${repository.node.primaryLanguage === null ? 'none' : "flex" };" class="most-used-tech">
+                            <div class="tech-color" style="background-color: ${repository.node.primaryLanguage === null ? '#fff' : repository.node.primaryLanguage.color };"></div>
+                            <h5 class="tech-name">${repository.node.primaryLanguage === null ? "Unavailable" : repository.node.primaryLanguage.name}</h5>
+                        </div>
+                        <div class="stars">
+                            <div class="star-icon">
+                                <img src="assets/icons/clarity_star-line.svg"/>
+                            </div>
+                            <h5 class="star-count">${repository.node.stargazerCount}</h5>
+                        </div>
+                        <div class="forks">
+                            <div class="fork-icon">
+                                <img src="assets/icons/jam_fork.svg"/>
+                            </div>
+                            <h5 class="forks-count">${repository.node.forkCount}</h5>
+                        </div>
+                        <p class="last-update">Updated on ${updated_at_time.toLocaleDateString('en-US',{month:'long', day:'2-digit', year:'numeric',hour:'2-digit'})}</p>
+                    </div>
+                </div>
+                <div class="star-repo">
+                    <button class="star-repo-btn">
+                        <img src="assets/icons/clarity_star-line.svg"/>
+                        <h3>Star</h3>
+                    </button>
+                </div>
+            `
+
+            // Append to Repositories list
+            repoListItem.innerHTML = repositoryDetails;
+            repoList.appendChild(repoListItem);
+        })
+        
+        // Add to DOM
+        userRepositories.appendChild(repoList);
+    })
+})
--- js/userRequest.js
+++ js/userRequest.js
@@ -21,13 +21,9 @@ const query =
             node {
                 ... on User {
                     id
-                    bio
                     avatarUrl
                     login
                     name
-                    bioHTML
-                    email
-                    url
                 }
             }
         }
@@ -120,22 +116,30 @@ searchInput.addEventListener("input",(e)=>{
             filteredResults.sort().map((result)=>{
                 let searchResult = result.node;
                 let { id,avatarUrl,login,name } = searchResult;
-                const searchResultListItem = `
-                    <li id="${id}" class="search-result">
-                        <a class="search-result">
-                            <div class="avatar">
-                                <img src="${avatarUrl}"/>
-                            </div>
-                            <div class="user">
-                                <h3 class="name">${name}</h3>
-                                <p class="login">${login}</p>
-                            </div>
-                        </a>
-                    </li>
+
+                const searchResultListItem = document.createElement("li");
+                searchResultListItem.setAttribute("id",id);
+                searchResultListItem.setAttribute("class","search-result");
+                searchResultListItem.addEventListener("click",()=>storeSearchResult(login));
+
+                const searchResultListItemLink = `
+                    <a class="search-result">
+                        <div class="avatar">
+                            <img src="${avatarUrl}"/>
+                        </div>
+                        <div class="user">
+                            <h3 class="name">${name}</h3>
+                            <p class="login">${login}</p>
+                        </div>
+                    </a>
                 `
-                searchResultList.innerHTML = searchResultListItem 
+                searchResultListItem.innerHTML = searchResultListItemLink
+                searchResultList.appendChild(searchResultListItem)
             })
             searchResults.appendChild(searchResultList)
+
+            // Store Results in Session storage
+            sessionStorage.setItem("users",JSON.stringify(edges));
             
         }else if(edges.length === 0){
             console.log("empty")
@@ -157,6 +161,80 @@ searchInput.addEventListener("input",(e)=>{
     }
     
 })
+
+const storeSearchResult = (id) =>{
+    sessionStorage.setItem("user",JSON.stringify(id))
+}
+
+
+// searchInput.addEventListener("input",(e)=>{
+//     e.preventDefault();
+
+//     console.log(searchInput.value)
+
+//     searchResults.style.display = 'block';
+    
+
+//     axios.post(
+//         githubUrl,
+//         { 
+//             query: query,
+//             variables: { "queryString": JSON.stringify(searchInput.value)}
+//         }, 
+//         {headers: auth}
+//     )
+//     .then(function (response) {
+//         const { edges } = response.data.data.search
+//         const searchResultList = document.createElement("ul");
+//         console.log(response.data)
+
+//         if(edges.length > 0){
+//             const filteredResults = edges.filter(result=>{
+//                 const searchValue = searchInput.value.toLowerCase();
+//                 const userNameMatches = result.node.login.toLowerCase().includes(searchValue);
+//                 return userNameMatches;
+//             })
+
+//             filteredResults.sort().map((result)=>{
+//                 let searchResult = result.node;
+//                 let { id,avatarUrl,login,name } = searchResult;
+//                 const searchResultListItem = `
+//                     <li id="${id}" class="search-result">
+//                         <a class="search-result">
+//                             <div class="avatar">
+//                                 <img src="${avatarUrl}"/>
+//                             </div>
+//                             <div class="user">
+//                                 <h3 class="name">${name}</h3>
+//                                 <p class="login">${login}</p>
+//                             </div>
+//                         </a>
+//                     </li>
+//                 `
+//                 searchResultList.innerHTML = searchResultListItem 
+//             })
+//             searchResults.appendChild(searchResultList)
+            
+//         }else if(edges.length === 0){
+//             console.log("empty")
+//         }
+        
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     });
+
+//     // Clear state
+//     if(searchInput.value.length === 0){
+//         searchForm.reset();
+//         searchResults.style.display = 'none';
+//     }
+
+//     if(searchInput.value === ""){
+//         clearSearchBtn.style.display = "none";
+//     }
+    
+// })
 
 document.querySelectorAll(".search-result").forEach((searchResult) =>{
     searchResult.addEventListener('click',(e)=>{
