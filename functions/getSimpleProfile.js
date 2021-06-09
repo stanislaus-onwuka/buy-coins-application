209dca47af6560238d62b5dab5891baaab40784d
stanislaus-onwuka <kachi2505@yahoo.com>
2021-06-09 22:20:54 +0000

    SI

--- functions/getSimpleProfile.js
+++ functions/getSimpleProfile.js
@@ -1,4 +1,9 @@
 const axios = require("axios");
+const dotenv = require("dotenv");
+
+if (process.env.NODE_ENV !== "production") {
+    dotenv.config();
+}
 
 const { TOKEN } = process.env;
 const githubUrl = 'https://api.github.com/graphql';
@@ -24,14 +29,11 @@ const query =
 
 exports.handler = async(event,context) =>{
     try {
-        console.log("event", event);
-        
-        const {data} = await axios.post(
+        const { data } = await axios.post(
             githubUrl,
             { 
                 query: query,
                 variables: { "queryString": event.queryStringParameters.username}
-
             }, 
             {headers:{
                 "Authorization": `Bearer ${TOKEN}`,
@@ -41,10 +43,10 @@ exports.handler = async(event,context) =>{
     
         return{
             statusCode:200,
-            body: data
+            body: JSON.stringify(data)
         }
     } catch (error) {
-        console.error(error);
+        console.error("error.response.data", error.response.data);
 
         return {
             statusCode: 500,
